546c5b123e57a98a8c161e2455e875c1d2b5abcc
Grécio Diogo <fonebahia8@gmail.com>
2025-07-21 13:00:17 +0100

    fix: change get client ip configs

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ import os
 
 app = Flask(__name__)
 
-def get_client_hostname():
-    return socket.gethostname()
-
 def get_client_ip():
-    return request.remote_addr
+    return request.headers.get('X-Forwarded-For', request.remote_addr)
+
+def get_client_hostname():
+    ip = get_client_ip()
+    try:
+        return socket.gethostbyaddr(ip)[0]
+    except socket.herror:
+        return "Unknown"
 
 @app.route('/')
 def get_client_info():
-    hostname = get_client_hostname()
     ip = get_client_ip()
+    hostname = get_client_hostname()
     return jsonify({"Hostname": hostname, "IP": ip})
 
 if __name__ == '__main__':
